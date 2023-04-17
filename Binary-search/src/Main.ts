9b0947d56ff0363f829a30eb7ea48ae58b5e9209
Hyboy95 <temilator@gmail.com>
2023-04-17 23:58:58 +0700

    second commit

--- Binary-search/src/Main.ts
+++ Binary-search/src/Main.ts
@@ -3,10 +3,10 @@ import {BinaryTree} from "./BinaryTree";
 let tree = new BinaryTree<number>();
 tree.insert(20);
 tree.insert(80);
+tree.insert(15);
 tree.insert(70);
 tree.insert(89);
 tree.insert(102);
-tree.insert(15);
 tree.insert(18);
 tree.insert(17);
 tree.insert(19);
@@ -14,7 +14,7 @@ tree.insert(13);
 tree.insert(14);
 
 
-console.log(tree.deleteData(15));
-console.log(tree.findParentOfNode(13));
+console.log(tree.deleteData(70));
+// console.log(tree.findParentOfNode(80));
 tree.inorder(tree.root);
 // console.log(tree.totalNode);
